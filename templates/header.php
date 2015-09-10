90f2d8e4f589b49366f40f6731acdbdb80a286cf
Greg Opperman <gopperman@gmail.com>
2015-09-10 09:38:27 -0400

    Nav changes

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <header class="banner" role="banner">
-  <div class="container">
+  <div class="container-fluid">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav role="navigation">
       <?php
